1d7887fc0749b190275d98eea34bbfd6da57d76b
Jminding <69938575+Jminding@users.noreply.github.com>
2021-03-22 22:00:44 -0400

    Add files via upload

--- AoPSBotCode.js
+++ AoPSBotCode.js
@@ -1,7 +1,7 @@
 let Discord = require("discord.js");
 let client = new Discord.Client();
-
-client.login("No I'm not putting the token in this code");
+const TOKEN = process.env.TOKEN;
+client.login(TOKEN);
 
 client.on("message", message => {
   if (message.content === "aops invite") {
@@ -11,7 +11,7 @@ client.on("message", message => {
 client.on("message", message => {
   if (message.content === "aops botinfo") {
     return message.channel.send(
-        "AoPSBot is a utilities bot created by Synthetic#0920 and worked on by Jminding#6728 specifically for the AoPS discord server. To get started, type a command!"
+      "AoPSBot is a utilities bot created by Synthetic#0920 and worked on by Jminding#6728 specifically for the AoPS discord server. To get started, type a command!"
     );
   }
 });
@@ -25,10 +25,10 @@ client.on("message", message => {
     message.channel.send("Connecting to server").then(async msg => {
       msg.delete();
       message.channel.send(
-          `Latency is ${msg.createdTimestamp -
+        `Latency is ${msg.createdTimestamp -
           message.createdTimestamp}ms. API Latency is ${Math.round(
-              client.ws.ping
-          )}ms`
+          client.ws.ping
+        )}ms`
       );
     });
   }
@@ -40,12 +40,16 @@ client.on("message", message => {
 });
 client.on("message", message => {
   if (message.content === "aops tos") {
-    return message.channel.send("Discord TOS: https://discord.com/terms\nAoPS TOS: https://artofproblemsolving.com/company/tos");
+    return message.channel.send(
+      "Discord TOS: https://discord.com/terms\nAoPS TOS: https://artofproblemsolving.com/company/tos"
+    );
   }
 });
 client.on("message", message => {
   if (message.content === "aops rules") {
-    return message.channel.send("Check <#796544244839088129> for the rules of this server!");
+    return message.channel.send(
+      "Check <#796544244839088129> for the rules of this server!"
+    );
   }
 });
 client.on("message", message => {
@@ -61,7 +65,7 @@ client.on("message", message => {
 client.on("message", message => {
   if (message.content === "aops duplication") {
     return message.channel.send(
-        "https://cdn.discordapp.com/attachments/636679472527179788/788080706697691156/duplication.webm"
+      "https://cdn.discordapp.com/attachments/636679472527179788/788080706697691156/duplication.webm"
     );
   }
 });
@@ -78,57 +82,51 @@ client.on("message", message => {
 client.on("message", message => {
   if (message.content === "aops support") {
     return message.channel.send(
-        "Contact Synthetic#0920 or Jminding#6728 for help!"
+      "Contact Synthetic#0920 or Jminding#6728 for help!"
     );
   }
 });
 client.on("message", message => {
   if (message.content === "aops about") {
     return message.channel.send(
-        "AoPS provides math and programming classes for students grades 2-12 after school, weekends, and summer, with locations all over the USA."
+      "AoPS provides math and programming classes for students grades 2-12 after school, weekends, and summer, with locations all over the USA."
     );
   }
 });
 client.on("message", message => {
   if (message.content === "good") {
-    return message.channel.send(
-        "nice"
-    );
+    return message.channel.send("nice");
   }
 });
 client.on("message", message => {
   if (message.content === "bot prefix") {
-    return message.channel.send(
-        "aops"
-    );
+    return message.channel.send("aops");
   }
 });
 client.on("message", message => {
   if (message.content === "bad") {
-    return message.channel.send(
-        "lol n0ob"
-    );
+    return message.channel.send("lol n0ob");
   }
 });
 client.on("message", message => {
   if (message.content === "aops help" || message.content === "aops cmds") {
     return message.channel.send(
-        "AoPSBot here!  Here are my commands: \n`help` or `cmds` - Shows this message that has the commands\n`invite` - Gives you the invite link to this server\n`botinfo` - Gives you info about the bot\n`break` - The bot will say that it's broken\n`rules` - The bot will show you the rules of this server\n`ping` - Shows you the ping of the bot\n`pong` - Just says \"🏓 Ping!\"\n`test` - Tests if the bot is working\n`website` - Gives you the link to the AoPS website\n`bump` - Bumps the channel\n`support` - Tells you who to contact for support\n`about` - Tells you what AoPS is about\n`prefix` - Returns the prefix of the bot (which is aops)"
+      'AoPSBot here!  Here are my commands: \n`help` or `cmds` - Shows this message that has the commands\n`invite` - Gives you the invite link to this server\n`botinfo` - Gives you info about the bot\n`break` - The bot will say that it\'s broken\n`rules` - The bot will show you the rules of this server\n`ping` - Shows you the ping of the bot\n`pong` - Just says "🏓 Ping!"\n`test` - Tests if the bot is working\n`website` - Gives you the link to the AoPS website\n`bump` - Bumps the channel\n`support` - Tells you who to contact for support\n`about` - Tells you what AoPS is about\n`prefix` - Returns the prefix of the bot (which is aops)'
     );
   }
 });
-client.on("ready", () =>{
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({
-        status: "online",  //You can show online, idle....
-        game: {
-            name: "Say \"aops help\" to see my commands!",  //The message shown
-            type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }
-    });
- });
-const http = require('http');
-const express = require('express');
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: "online", //You can show online, idle....
+    game: {
+      name: 'aops.com | aops help', //The message shown
+      type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
+    }
+  });
+});
+const http = require("http");
+const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
